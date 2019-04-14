decd52dfe58cadabb33a6fd9a4cc8c0f1fc11ca1
llu13701 <49617749+llu13701@users.noreply.github.com>
2019-04-14 14:19:31 -0400

    Update wikiconnect.py

--- wikiconnect.py
+++ wikiconnect.py
@@ -6,17 +6,24 @@ Created on Sat Apr 13 18:17:37 2019
 @author: louisalu
 """
 
-import pandas as pd
+#import pandas as pd
+import os
 import dask.dataframe as dd
 
+#change the directory and also the file name
+os.chdir('/Users/louisalu/Documents/wikiproject')
 
-readpage = dd.read_csv('wikipage.csv', sep='\t', lineterminator='\r', header=None, usecols=[0,2])
-readtopic = dd.read_csv('topiccat.csv', sep='\t', lineterminator='\r', header=None, usecols=[0, 1])
+readpage = dd.read_csv('pageoutput2.csv', sep='\t', header=None, usecols=[0,2])
+readtopic = dd.read_csv('topicoutput2.csv', sep='\t', header=None, usecols=[0, 1])
 
 readpage.columns=['col1', 'col2']
 readtopic.columns=['col1', 'col2']
 
-results=dd.merge(left=readtopic, right=readpage, how='left', on='col1')
+#results=dd.merge(left=readtopic, right=readpage, how='left', on='col1')
 
-results.to_csv('file3.csv', index=False)
+results=dd.merge(left=readpage, right=readtopic, how='right', on='col1')
+
+results.columns=['id', 'title', 'cat']
+
+results.to_csv(r'*.csv', sep='\t', header=True, index=False)
 
