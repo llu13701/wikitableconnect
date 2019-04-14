4504899ff188317149aa37b8ebebfa0009683919
llu13701 <49617749+llu13701@users.noreply.github.com>
2019-04-14 13:24:39 -0400

    Update wikiconnect.py

--- wikiconnect.py
+++ wikiconnect.py
@@ -7,11 +7,16 @@ Created on Sat Apr 13 18:17:37 2019
 """
 
 import pandas as pd
+import dask.dataframe as dd
 
-readpage = pd.read_csv('wikipage.csv', delimiter=',', header=None, usecols=[0,2])
-readtopic = pd.read_csv('topiccat.csv', delimiter=',', header=None, usecols=[0, 1])
+
+readpage = dd.read_csv('wikipage.csv', sep='\t', lineterminator='\r', header=None, usecols=[0,2])
+readtopic = dd.read_csv('topiccat.csv', sep='\t', lineterminator='\r', header=None, usecols=[0, 1])
 
 readpage.columns=['col1', 'col2']
 readtopic.columns=['col1', 'col2']
 
-results=pd.merge(left=readtopic, right=readpage, how='left', on='col1')
+results=dd.merge(left=readtopic, right=readpage, how='left', on='col1')
+
+results.to_csv('file3.csv', index=False)
+
