d02d24cc2f831f11e86989ee1a78f5ce0e0d3d81
llu13701 <49617749+llu13701@users.noreply.github.com>
2019-04-23 19:16:41 -0400

    Update keywords_extractions.py

--- keywords_extractions.py
+++ keywords_extractions.py
@@ -30,6 +30,21 @@ def preprocess(sent):
     sent=nltk.pos_tag(sent)
     return sent
 
+def tokenize(text, pat='(?u)\\b\\w\\w+\\b', stop_words='english', min_len=2):
+    if stop_words:
+        stop = set(stopwords.words(stop_words))
+    return [w
+            for w in regexp_tokenize(text.casefold(), pat)
+            if w not in stop and len(w) >= min_len]
+
+# so training words distribution with the 20newsgroups
+def get_data(): 
+    twenty_train = \
+        fetch_20newsgroups(subset='all', shuffle=True)
+    
+    X_train = pd.DataFrame(twenty_train.data, columns=['text'])
+    return X_train
+
 
 
 def main():
@@ -44,6 +59,20 @@ def main():
         # relate all the special character
         question=question.str.replace(r'\W+', ' ')
         question=question.to_string()
-        # using nltk
+        # using nltk to find noun and so
         question_pos_analysis = preprocess(question)
         ner_one=nlp(question)
+        
+        #X-train represents the articles from all category
+        X_train = get_data()
+        words = tokenize(question, min_len=3)
+        # remove the "question:
+        words.remove('question')
+        
+        for i in words:
+            #single_word=words[i]
+            single_word='mblawson' # this is just for testing
+            
+
+
+
