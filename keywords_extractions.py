ee39054152028b2a3feb577936b1b7061f5815a4
llu13701 <49617749+llu13701@users.noreply.github.com>
2019-04-23 18:16:12 -0400

    Update keywords_extractions.py

--- keywords_extractions.py
+++ keywords_extractions.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+
+#https://stackoverflow.com/questions/49500259/how-to-find-most-frequently-used-words-used-on-data-using-python
 """
 Created on Tue Apr 23 13:41:15 2019
-
 @author: louisalu
 """
 
@@ -10,12 +12,28 @@ import os
 import pandas as pd
 import spacy
 from spacy import displacy
-from collections import Counter
 import en_core_web_sm
-nlp=en_core_web_sm.load()
+nlp = en_core_web_sm.load()
+
+from collections import Counter
+import nltk
+from nltk import word_tokenize, pos_tag, ne_chunk, punkt,sent_tokenize,regexp_tokenize,FreqDist
+from nltk.corpus import stopwords
+from sklearn.datasets import fetch_20newsgroups
+from wordcloud import WordCloud, STOPWORDS
+
+
+
+#preprocessing the questions for POS analysis
+def preprocess(sent):
+    sent=nltk.word_tokenize(sent)
+    sent=nltk.pos_tag(sent)
+    return sent
+
+
 
 def main():
-    os.chdir('/Users/louisalu/Documents/wikiproject')
+    os.chdir('/Users/amit/Desktop/wikitableconnect')
     question_file_name='question_testing_sample.csv'
     sample_question=pd.read_csv(question_file_name, header=None, encoding='latin1')
     sample_question.columns=['Question']
@@ -25,5 +43,7 @@ def main():
         question=sample_question.iloc[i]
         # relate all the special character
         question=question.str.replace(r'\W+', ' ')
-
-        
+        question=question.to_string()
+        # using nltk
+        question_pos_analysis = preprocess(question)
+        ner_one=nlp(question)
